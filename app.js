ac7f1d2a98dc645c19ebce0e468c7b095f215d57
Jordan <jordandevaney28@gmail.com>
2020-11-05 21:38:27 -0500

    add helmetjs for security

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let cors = require('cors');
-// let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let helmet = require('helmet')
 
 let classesRoute = require('./api/classes/classesRoutes');
 let clientsRoute = require('./api/clients/clientsRoute');
@@ -12,13 +12,12 @@ let app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cors());
-// app.use(express.urlencoded({ extended: false }));
+app.use(helmet())
 
 app.use('/api/classes', classesRoute);
 app.use('/api/clients', clientsRoute);
 app.use('/api/instructors', instructorsRoute);
 
-/* GET home page. */
 app.get('/', function(req, res) {
   res.status(200).send("app is up");
 });
